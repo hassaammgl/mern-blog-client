68a6ff15e5aabb59b0441e2fc766a718053a41b2
hassaammgl <h455a4mmehtab@gmail.com>
2024-01-22 03:28:18 +0500

    colors are added in website

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { FaRegBell } from "react-icons/fa6";
 
 const Header = () => {
   return (
     <div className="header">
         <img src="companylogo.png" className="img"  alt="" />
+        <div className="rightheader">
+            <FaRegBell />
+        </div>
     </div>
   )
 }
