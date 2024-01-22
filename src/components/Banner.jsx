0d20e412ca5d877be13190b623ef9f8d2947153b
hassaammgl <h455a4mmehtab@gmail.com>
2024-01-22 19:34:36 +0500

    ameen

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-
+import React from 'react';
+import { IoIosClock } from "react-icons/io";
 const Banner = () => {
   return (
     <div className='banner'>
         <div className="bannercontent">
             <button>Communication & Marketing</button>
             <h2>The Definitive Guide to AI for Nonprofits</h2>
+            <div className="clock">
+            <IoIosClock className='icon' /> 
+            <p>5 min</p>
+            </div>
         </div>
     </div>
   )
