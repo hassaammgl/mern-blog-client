e3418dec7da852ec7630b09fefa8a82255360819
hassaammgl <h455a4mmehtab@gmail.com>
2024-01-22 19:59:58 +0500

    dsafasdf

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { PiClockFill } from "react-icons/pi";const Banner = () => {
+import { PiClockFill } from "react-icons/pi";
+import girl from "../../public/girl.jpg"
+
+const Banner = () => {
   return (
     <div className='banner'>
         <div className="bannercontent">
@@ -8,6 +11,9 @@ import { PiClockFill } from "react-icons/pi";const Banner = () => {
             <div className="clock">
             <PiClockFill className='icon' /> 
             <p>5 min</p>
+            </div>
+            <div className="review">
+                <img src={girl} className='girl'  alt="" />
             </div>
         </div>
     </div>
