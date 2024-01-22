5c3d0a2851870f6ff470356b706a51384c85c4b7
hassaammgl <h455a4mmehtab@gmail.com>
2024-01-22 18:42:39 +0500

    dasfdsa

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "../src/styles/utils.scss";
 import "../src/styles/header.scss";
+import "../src/styles/banner.scss";
 import Header from "./components/Header"
+import Banner from "./components/Banner";
 
 function App() {
 
   return (
     <>
     <Header />
+    <Banner />
     </>
   )
 }
