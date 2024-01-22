b3ea06c726c2193c30461a6f69bbce51d1477223
hassaammgl <h455a4mmehtab@gmail.com>
2024-01-22 20:03:41 +0500

    dsafsdf

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
+import React from "react";
 import { PiClockFill } from "react-icons/pi";
-import girl from "../../public/girl.jpg"
+import girl from "../../public/girl.jpg";
 
 const Banner = () => {
   return (
-    <div className='banner'>
-        <div className="bannercontent">
-            <button>Communication & Marketing</button>
-            <h2>The Definitive Guide to AI for Nonprofits</h2>
-            <div className="clock">
-            <PiClockFill className='icon' /> 
-            <p>5 min</p>
-            </div>
-            <div className="review">
-                <img src={girl} className='girl'  alt="" />
-            </div>
+    <div className="banner">
+      <div className="bannercontent">
+        <button>Communication & Marketing</button>
+        <h2>The Definitive Guide to AI for Nonprofits</h2>
+        <div className="clock">
+          <PiClockFill className="icon" />
+          <p>5 min</p>
         </div>
+        <div className="review">
+          <img src={girl} className="girl" alt="" />
+          <p>Hassaan</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
