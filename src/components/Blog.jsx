ca94541e04a1f6f6a3a46f9ee531122651bc4ecc
hassaammgl <h455a4mmehtab@gmail.com>
2024-01-23 04:08:19 +0500

    dasfdaf

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import prog from "../../public/prog.jpg"
 
 const Blog = () => {
   return (
     <div className='blog'>
         <h2>Welcome to DevLogs Blog!</h2>
-        
     </div>
   )
 }
