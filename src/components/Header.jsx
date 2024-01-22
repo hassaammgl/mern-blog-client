84dccf41b26c1fffd20b73eeca76036a015e6eb4
hassaammgl <h455a4mmehtab@gmail.com>
2024-01-22 18:29:04 +0500

    dsa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
+import React from "react";
 import { FaRegBell } from "react-icons/fa6";
 
 const Header = () => {
   return (
     <div className="header">
-        <h2>DevLogs.</h2>
-        {/* <img src="companylogo.png" className="img"  alt="" /> */}
-        <div className="rightheader">
-            <FaRegBell className="icon" />
-            <p>Discover DevLogs</p>
-            <button>Try DevLogs</button>
-        </div>
+      <h2>DevLogs.</h2>
+      <div className="rightheader">
+        <FaRegBell className="icon" />
+        <p>Discover DevLogs</p>
+        <button>Try DevLogs</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
