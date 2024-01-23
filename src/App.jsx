e75fb1e28e7bb39f2e210a3b1eea06a50cd8a0f0
hassaammgl <h455a4mmehtab@gmail.com>
2024-01-23 15:19:53 +0500

    dsafdasf

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import "../src/styles/banner.scss";
 import Blog from "./components/Blog";
 import Header from "./components/Header"
 import Banner from "./components/Banner";
+import "../src/styles/blogcontainer.scss";
+import BlogContainer from "./components/BlogContainer";
 
 function App() {
 
