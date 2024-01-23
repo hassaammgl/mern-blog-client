f3f9d1502e55865201004e38ce37dae3e922ae47
hassaammgl <h455a4mmehtab@gmail.com>
2024-01-23 14:14:44 +0500

    adsfdasf

--- src/components/BlogContainer.jsx
+++ src/components/BlogContainer.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import prog from "../../public/prog.jpg"
+import React from "react";
+import prog from "../../public/prog.jpg";
 
 const BlogContainer = () => {
   return (
-    <div>BlogContainer</div>
-  )
-}
+    <div className="blogcontainer">
+      <div className="top">
+        <img src={prog} className="img"  alt="" />
+      </div>
+      <div className="bottom">
+        <button>hassaam</button>
+      </div>
+    </div>
+  );
+};
 
-export default BlogContainer
+export default BlogContainer;
