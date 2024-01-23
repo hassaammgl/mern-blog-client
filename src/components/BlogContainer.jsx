2065447e1af7b42d81c0029b9567a5a3b690404b
hassaammgl <h455a4mmehtab@gmail.com>
2024-01-23 14:39:01 +0500

    afsdaf

--- src/components/BlogContainer.jsx
+++ src/components/BlogContainer.jsx
@@ -9,6 +9,7 @@ const BlogContainer = () => {
       </div>
       <div className="bottom">
         <button>hassaam</button>
+        <h2>The Definitive Guide to AI for Nonprofits</h2>
       </div>
     </div>
   );
